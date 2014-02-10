c2f19f066ee9a321708e2f124e558bd67d901c7f
Chris Hynes <chris@programcsharp.com>
2014-02-10 11:23:38 -0700

    Broke mvc results into separate project

--- Deployment/publish-flat-files.bat
+++ Deployment/publish-flat-files.bat
@@ -1,4 +1,5 @@
 msbuild ../FlatFiles.sln /p:Configuration=Release
 nuget pack ../FlatFiles/FlatFiles.csproj -Prop Configuration=Release
+nuget pack ../FlatFiles.Mvc/FlatFiles.Mvc.csproj -Prop Configuration=Release
 nuget push *.nupkg
 del *.nupkg
