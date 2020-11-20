87275654104dbce55e8ed09f8904036c57a72388
Travis Parks <jehugaleahsa@gmail.com>
2020-11-19 19:16:16 -0500

    Clean up some DataTable tests.

--- FlatFiles.Test/DataTableExtensionsTester.cs
+++ FlatFiles.Test/DataTableExtensionsTester.cs
@@ -316,15 +316,7 @@ namespace FlatFiles.Test
         {
             DataTable table = new DataTable();
             DataColumn idColumn = table.Columns.Add("id", typeof(int));
-            DataColumn nameColumn = table.Columns.Add("name", typeof(string));
-            DataColumn createdColumn = table.Columns.Add("created", typeof(DateTime));
-            DataColumn avgColumn = table.Columns.Add("avg", typeof(decimal));
             table.Constraints.Add("PK_blah", idColumn, true);
-
-            DataRow bobRow = table.Rows.Add(new object[] { 1, "Bob", new DateTime(2018, 07, 16), 12.34m });
-            DataRow johnRow = table.Rows.Add(new object[] { 2, "John", new DateTime(2018, 07, 17), 23.45m });
-            DataRow susanRow = table.Rows.Add(new object[] { 3, "Susan", new DateTime(2018, 07, 18), 34.56m });
-            DataRow nullRow = table.Rows.Add(new object[] { 4, null, null, null });
 
             var options = new SeparatedValueOptions()
             {
@@ -356,14 +348,7 @@ namespace FlatFiles.Test
         {
             DataTable table = new DataTable();
             DataColumn idColumn = table.Columns.Add("id", typeof(int));
-            DataColumn nameColumn = table.Columns.Add("name", typeof(string));
-            //DataColumn createdColumn = table.Columns.Add("created", typeof(DateTime));
-            DataColumn avgColumn = table.Columns.Add("avg", typeof(decimal));
             table.Constraints.Add("PK_blah", idColumn, true);
-
-            DataRow bobRow = table.Rows.Add(new object[] { 1, "Bob", 12.34m });
-            DataRow johnRow = table.Rows.Add(new object[] { 2, "John", 23.45m });
-            DataRow susanRow = table.Rows.Add(new object[] { 3, "Susan", 34.56m });
 
             var options = new SeparatedValueOptions() { IsFirstRecordSchema = true };
             var stringWriter = new StringWriter();
@@ -389,14 +374,7 @@ namespace FlatFiles.Test
         {
             DataTable table = new DataTable();
             DataColumn idColumn = table.Columns.Add("id", typeof(int));
-            DataColumn nameColumn = table.Columns.Add("name", typeof(string));
-            DataColumn createdColumn = table.Columns.Add("created", typeof(DateTime));
-            DataColumn avgColumn = table.Columns.Add("avg", typeof(decimal));
             table.Constraints.Add("PK_blah", idColumn, true);
-
-            DataRow bobRow = table.Rows.Add(new object[] { 1, "Bob", new DateTime(2018, 07, 16), 12.34m });
-            DataRow johnRow = table.Rows.Add(new object[] { 2, "John", new DateTime(2018, 07, 17), 23.45m });
-            DataRow susanRow = table.Rows.Add(new object[] { 3, "Susan", new DateTime(2018, 07, 18), 34.56m });
 
             var options = new SeparatedValueOptions() { IsFirstRecordSchema = true };
             var stringWriter = new StringWriter();
@@ -454,14 +432,7 @@ namespace FlatFiles.Test
         {
             DataTable table = new DataTable();
             DataColumn idColumn = table.Columns.Add("id", typeof(int));
-            DataColumn nameColumn = table.Columns.Add("name", typeof(string));
-            DataColumn createdColumn = table.Columns.Add("created", typeof(DateTime));
-            DataColumn avgColumn = table.Columns.Add("avg", typeof(decimal));
             table.Constraints.Add("PK_blah", idColumn, true);
-
-            DataRow bobRow = table.Rows.Add(new object[] { 1, "Bob", new DateTime(2018, 07, 16), 12.34m });
-            DataRow johnRow = table.Rows.Add(new object[] { 2, "John", new DateTime(2018, 07, 17), 23.45m });
-            DataRow susanRow = table.Rows.Add(new object[] { 3, "Susan", new DateTime(2018, 07, 18), 34.56m });
 
             var options = new SeparatedValueOptions()
             {
